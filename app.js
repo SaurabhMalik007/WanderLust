e1b6ad35f1acbb7be6abc8cede3f420d9031e931
SaurabhMalik007 <saurabh.malikk2@gmail.com>
2024-11-21 18:15:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ main().then(()=>{
 .catch((err)=>{ console.log(err)});
 
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
-    // await mongoose.connect(dbUrl);
+    // await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect(dbUrl);
 }
 
 
